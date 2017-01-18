0c544d566921356b6107422750cf95dae9f1b5ff
Teddy <i.kenting@gmail.com>
2017-01-18 22:20:04 +0800

    code refactor for console interact.

--- preview/peopleinteract_update.py
+++ preview/peopleinteract_update.py
@@ -15,21 +15,14 @@ while True:
     try:
         record = db[key]
     except:
-        new_record = Person('?', '?')
-        #print 'No such key "%s"' % key
-        for field in fieldnames:
-            print '[%s]=%s' % (field, getattr(new_record, field))
-            v = raw_input('\tnew?=> ')
-            if v:
-                setattr(new_record, field, eval(v))
-                db[key] = new_record
-    else:
-        for field in fieldnames:
-            print '[%s]=%s' % (field, getattr(record, field))
-            v = raw_input('\tnew?=> ')
-            if v:
-                setattr(record, field, eval(v))
-                db[key] = record
+        record = Person('?', '?')
+    for field in fieldnames:
+        print '[%s]=%s' % (field, getattr(record, field))
+        v = raw_input('\tnew?=> ')
+        if v:
+            setattr(record, field, eval(v))
+            db[key] = record
     key = raw_input('key? => ')
 
+print 'Bye...!'
 db.close()
